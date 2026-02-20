dbe3661190ae14fc2ba3f2c6c340ca23f00216bd
ssreyagade <ssreyaswapnilgade@gmail.com>
2026-02-20 17:32:30 +0530

    Add files via upload

---
+++ UseContext/App.jsx
@@ -0,0 +1,13 @@
+// App.js
+import React from "react";
+import ParentComp from "./components/ParentComp";
+
+function App() {
+  return (
+    <div className="App">
+      <ParentComp />
+    </div>
+  );
+}
+
+export default App;

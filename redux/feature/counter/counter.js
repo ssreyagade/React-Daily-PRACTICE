8a03cc4d43de1bb2c6079972d90580e732d5bed6
ssreyagade <ssreyaswapnilgade@gmail.com>
2026-03-14 22:36:18 +0530

    Add files via upload

---
+++ redux/feature/counter/counter.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement, reset } from "./counterSlice";
+
+function Counter() {
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <h1>Redux Counter</h1>
+
+      <h2>{count}</h2>
+
+      <button onClick={() => dispatch(increment())}>Increment</button>
+
+      <button onClick={() => dispatch(decrement())}>Decrement</button>
+
+      <button onClick={() => dispatch(reset())}>Reset</button>
+    </div>
+  );
+}
+
+export default Counter;

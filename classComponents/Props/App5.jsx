b5149aff35b27e2b82fb8ac76ae4fcbbe646fdab
ssreyagade <ssreyaswapnilgade@gmail.com>
2026-07-04 17:38:48 +0530

    Add files via upload

---
+++ classComponents/Props/App5.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App5 = (props) => {
+  return (
+    <div>
+      <h1>My name is {props.name}</h1>
+      <h2>My role is {props.desc}</h2>
+    </div>
+  );
+};
+
+export default App5;
